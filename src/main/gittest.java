4333c26fa3d70aa5f02b7913babee08221bf29c3
nwt <2421788377@qq.com>
2022-11-11 22:09:03 +0800

    third commit

--- src/main/gittest.java
+++ src/main/gittest.java
@@ -6,7 +6,6 @@ package main;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
+        System.out.println("hello git33333");
     }
 }
