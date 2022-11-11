3d79ea9e1bd8806eef6c6aac563b5fe56c0d5078
nwt <2421788377@qq.com>
2022-11-11 22:08:24 +0800

    second commit

--- src/main/gittest.java
+++ src/main/gittest.java
@@ -6,5 +6,7 @@ package main;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
